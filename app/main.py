9067898dc9dfa701b7ac8bfd311743a9ec8f6d34
Adelaja Qowiyyu <adexolamilekan46@gmail.com>
2023-10-02 11:39:15 +0100

    Added the Transcript code

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+
 """
-PROJECT NAME: Live Recorder Endpoint
+PROJECT NAME: Live Recorder Backend Endpoint
+
+STORAGE: AZURE CLOUD STORAGE 
 
 """
 
@@ -12,10 +15,10 @@ from azure.core.exceptions import ResourceNotFoundError
 import base64
 from dotenv import load_dotenv
 from datetime import datetime
-# from moviepy.editor import VideoFileClip
 from pathlib import Path
 from fastapi.middleware.cors import CORSMiddleware
-
+from moviepy.editor import VideoFileClip
+import whisper
 
 app = FastAPI()
 
@@ -62,10 +65,13 @@ class VideoResponse(BaseModel):
   modified_time: str
   content_type: str
   title: str
+  transcript_url:str
 
 class VideoRequest(BaseModel):
   folder_name: str
   blob_name: str 
+
+
 
 #CREATE RECORDING SESSION FOLDER ID
 @app.post("/api/start_recording", status_code=201)
@@ -90,18 +96,22 @@ async def start_recording() -> StartRecordingResponse:
  else:
   return StartRecordingResponse(status_code=422, message=FAILURE_MESSAGE)
 
+
+
 @app.post("/api/stream_recording", status_code=201)
 async def stream_recording(file: Chunk) -> StreamRecordingResponse:
     
-    print(file.folder_name, file.blob_name)
     file_data = base64.b64decode(file.data)
 
-
+    format = file.content_type.replace("video/", "")
+    
     folder_name = file.folder_name
     blob_name = file.blob_name
-    content_type = file.content_type
-    # format = content_type.replace("video/", "")
-    # VIDEO_LOCAL_PATH = f"{blob_name}.{format}"
+    content_type = format
+   
+    VIDEO_LOCAL_PATH = f"{blob_name}.{format}"
+
+    print(VIDEO_LOCAL_PATH)
 
     try:
 
@@ -127,19 +137,11 @@ async def stream_recording(file: Chunk) -> StreamRecordingResponse:
         
         blob_client.set_http_headers(blob_headers)
 
-
-        # Set the content type to "video/mp4" for the Append Blob
-        # blob_client.set_http_headers(content_type=content_type)
-
-        # blob_client.upload_blob(file_data, overwrite=True, content_settings=my_content_settings)
-
-
         # Read the binary data from the uploaded file and append it to the Append Blob
         blob_client.append_block(file_data, length=len(file_data))
 
-        # with open(VIDEO_LOCAL_PATH, "ab") as video_file:
-        #   # blob_data = blob_client.download_blob()
-        #   video_file.write(file_data)
+        with open(VIDEO_LOCAL_PATH, "ab") as video_file:
+          video_file.write(file_data)
     
         
         return StreamRecordingResponse(message= "Binary data appended successfully.", status_code=201)
@@ -149,8 +151,11 @@ async def stream_recording(file: Chunk) -> StreamRecordingResponse:
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
 
-@app.post("/api/stop_recording", status_code=200)
+
+
+@app.get("/api/stop_recording", status_code=200)
 async def stop_recording(file:  VideoRequest) -> VideoResponse:
   folder_name = file.folder_name
   blob_name = file.blob_name
@@ -164,138 +169,60 @@ async def stop_recording(file:  VideoRequest) -> VideoResponse:
 
     # Get or create an Append Blob client
     blob_client = container_client.get_blob_client(blob_name)
-
-    # Get the existing blob properties
-    properties = blob_client.get_blob_properties()
-
-    # Set the content_type and content_language headers, and populate the remaining headers from the existing properties
-    blob_headers = ContentSettings(content_type="video/mp4",
-                                  content_encoding=properties.content_settings.content_encoding,
-                                  content_language="en-US",
-                                  )
-    
-    blob_client.set_http_headers(blob_headers)
-
-    video_url = blob_client.url
     
     # Get the creation time of the blob (assuming the blob exists)
     creation_time = blob_client.get_blob_properties().creation_time.strftime("%Y-%m-%dT%H:%M:%SZ")
     modified_time = blob_client.get_blob_properties().last_modified.strftime("%Y-%m-%dT%H:%M:%SZ")
-    content_type = blob_client.get_blob_properties().content_settings.content_type
+    content_type = str(blob_client.get_blob_properties().content_settings.content_type)
+    video_url = blob_client.url
 
-    # Format the creation time as a string (e.g., as an ISO 8601 timestamp)
-    # creation_time = creation_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+    #Transcribing the Video
+    video_format = content_type.replace("video/", "")
 
-    return VideoResponse(status_code=200, message= SUCCESS_MESSAGE, url = video_url, 
-                         creation_time=creation_time, modified_time=modified_time, 
-                         content_type=content_type, title=blob_name)
+    video_file_path = f"{blob_name}.{video_format}"
+
+    if isinstance(video_file_path, Path):
+      video_file_path = str(video_file_path)
+
+    video_clip = VideoFileClip(video_file_path)
+
+    audio_clip = video_clip.audio
+
+    output_audio_file = f"{blob_name}.mp3"
+
+    audio_clip.write_audiofile(output_audio_file)
+
+    model = whisper.load_model("base")
+
+    result = model.transcribe(output_audio_file)
+
+    transcript_blob_name = f"transcript.txt" 
+    transcript_content_type = "text/plain"  
+
+    with open(transcript_blob_name, mode='w') as file:
+        transcript = result["text"]
+        file.write(transcript)
+
+    video_clip.close()
+    audio_clip.close()
+
+    # Get or create a Blob client
+    blob_client = container_client.get_blob_client(transcript_blob_name)
+
+    # Create the blob as a block blob (for text files)
+    blob_client.upload_blob(open(transcript_blob_name, "rb"), content_settings=ContentSettings(content_type=transcript_content_type))
+
+    
+    # Get the URL of the uploaded text file
+    transcript_url = blob_client.url
+
+    return VideoResponse(status_code=200, message= SUCCESS_MESSAGE, video_url = video_url, 
+                          creation_time=creation_time, modified_time=modified_time, 
+                          content_type=content_type, title=blob_name, transcript_url=transcript_url)
   
   except ResourceNotFoundError:
     raise HTTPException(status_code=404, detail=f"Blob '{blob_name}' not found")
 
-# @app.get("/api/transcribe")
-# async def transcribe_video(video: VideoRequest)->VideoResponse:
-#   folder_name = video.folder_name
-#   blob_name = video.blob_name
-
-#   print(folder_name, blob_name)
-#   SUCCESS_MESSAGE = "Video Transcribed Successfully"
-#   downloaded_video_path = "output_video.mp4"
-
-#   def transcribe_video():
-#     import whisper
-
-#     model = whisper.load_model("base")
-#     audio_path = "output_audio.mp3"
-#     result = model.transcribe(audio_path)
-
-#     with open("transcript.txt", mode='w') as file:
-#         transcript = result["text"]
-#         file.write(transcript)
-
-
-#   try:
-#       # Initialize Azure Blob Service Client
-#       blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
-
-#       # Get the blob client
-#       container_client = blob_service_client.get_container_client(folder_name)
-#       blob_client = container_client.get_blob_client(blob_name)
-
-#       if not blob_client.exists():
-#           raise HTTPException(status_code=404, detail=f"Blob '{blob_name}' not found")
-      
-#       # properties = blob_client.get_blob_properties()
-
-#       #   # Set the content_type and content_language headers, and populate the remaining headers from the existing properties
-#       #   blob_headers = ContentSettings(content_type=content_type,
-#       #                                 content_encoding=properties.content_settings.content_encoding,
-#       #                                 content_language="en-US",
-#       #                                 )
-        
-#       #   blob_client.set_http_headers(blob_headers)
-
-#       # # Set the content type and other headers
-#       # properties = blob_client.get_blob_properties()
-#       # blob_headers = {
-#       #     "content_type": "video/mp4",  # You can specify the desired video format
-#       #     "content_encoding": properties.content_settings.content_encoding,
-#       #     "content_language": "en-US",
-#       # }
-#       # blob_client.set_http_headers(blob_headers)
-
-#       # Download the blob to a local file
-#       with open(downloaded_video_path, "wb") as file:
-#           blob_data = blob_client.download_blob()
-#           file.write(blob_data.readall())
-      
-#       file.close()
-
-
-#       # # Perform transcription here (replace with your transcription logic)
-#       # transcription_result = "This is a placeholder transcription."
-
-#       # Return the response with the transcription result
-#       return VideoResponse(
-#           status_code=200,
-#           message=SUCCESS_MESSAGE,
-#           url=blob_client.url,
-#           creation_time=blob_client.get_blob_properties().creation_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
-#           modified_time=blob_client.get_blob_properties().last_modified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-#           content_type=blob_client.get_blob_properties().content_settings.content_type,
-#           title=blob_name,
-#           # transcription=transcription_result,
-#       )
-
-#   except HTTPException as e:
-#       raise e
-
-#   except Exception as e:
-#       raise HTTPException(status_code=500, detail=str(e))
-
-#   finally:
-#       pass
-#       # Clean up: Delete the downloaded video file
-#       # downloaded_video_path_obj = Path(downloaded_video_path)
-#       # if downloaded_video_path_obj.is_file():
-#       #     downloaded_video_path_obj.unlink()
-
-# async def extract_video_audio():
-
-#     # Ensure that video_file_path is a string
-#     if isinstance(video_file_path, Path):
-#         video_file_path = str(video_file_path)
-
-#     video_clip = VideoFileClip(video_file_path)
-
-#     audio_clip = video_clip.audio
-
-#     output_audio_file = "output_audio.mp3"
-
-#     audio_clip.write_audiofile(output_audio_file)
-
-#     video_clip.close()
-#     audio_clip.close()
 
 if __name__ == "__main__":
  import uvicorn
